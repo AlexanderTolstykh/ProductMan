2ad8f1cfced444684f7cabf190fb38859e37880a
AlexanderTolstykh <avt4157@gmail.com>
2018-03-31 15:37:36 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+;(function(){
+	'use strict';
+	$('.slider').slick({
+            autoplaySpeed: 10000,
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+        });
+})();
